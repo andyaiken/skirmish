dda1800e4ea2cffbd91ed7ed68d3a618883c7e25
Andy Aiken <aaiken@scottlogic.com>
2020-11-14 08:19:05 +0000

    Updates

---
+++ precache-manifest.e72757f7ef4884e710141fdfb17bd394.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73b1de29fb7dc50f7e56a3e146e02f5a",
+    "url": "/skirmish/index.html"
+  },
+  {
+    "revision": "ae2d3d947cfab8bf9ea0",
+    "url": "/skirmish/static/css/2.969b5337.chunk.css"
+  },
+  {
+    "revision": "e82589fb8e51e3a8add6",
+    "url": "/skirmish/static/css/main.362d1497.chunk.css"
+  },
+  {
+    "revision": "ae2d3d947cfab8bf9ea0",
+    "url": "/skirmish/static/js/2.bd993ff3.chunk.js"
+  },
+  {
+    "revision": "44ba416b9a7217ca99c025d9f4dd1124",
+    "url": "/skirmish/static/js/2.bd993ff3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e82589fb8e51e3a8add6",
+    "url": "/skirmish/static/js/main.5618bb77.chunk.js"
+  },
+  {
+    "revision": "53bd9ef2c77f52e12620",
+    "url": "/skirmish/static/js/runtime-main.0d4c2bb1.js"
+  }
+]);

8c064c42c142d4d6df84a414a32758b2c4fd766f
Andy Aiken <andy.aiken@live.co.uk>
2023-07-13 08:20:45 +0100

    Adjust help tab

--- src/components/modals/help/about-tab/about-tab.tsx
+++ src/components/modals/help/about-tab/about-tab.tsx
@@ -23,9 +23,10 @@ export class AboutTab extends Component<Props> {
 				<div className='about-tab'>
 					<Text>Skirmish designed by Andy Aiken; © Andy Aiken 2023</Text>
 					<hr />
+					<StatValue label='Version' value={pkg.version} />
+					<hr />
 					<StatValue label='Data size' value={this.props.game ? Format.toSize(JSON.stringify(this.props.game).length) : 0} />
 					<StatValue label='Options size' value={Format.toSize(JSON.stringify(this.props.options).length)} />
-					<StatValue label='Version' value={pkg.version} />
 				</div>
 			);
 		} catch {

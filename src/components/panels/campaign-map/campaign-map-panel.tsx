d256972dba3614439a079570e2d2a81f9126b24a
Andy Aiken <andy.aiken@live.co.uk>
2023-12-08 07:38:23 +0000

    Use SVG for campaign map

--- src/components/panels/campaign-map/campaign-map-panel.tsx
+++ src/components/panels/campaign-map/campaign-map-panel.tsx
@@ -2,7 +2,7 @@ import { Component } from 'react';
 
 import { CampaignMapLogic } from '../../../logic/campaign-map-logic';
 
-import type { CampaignMapModel, CampaignMapSquareModel } from '../../../models/campaign-map';
+import type { CampaignMapModel } from '../../../models/campaign-map';
 import type { RegionModel } from '../../../models/region';
 
 import './campaign-map-panel.scss';
@@ -14,14 +14,53 @@ interface Props {
 }
 
 export class CampaignMapPanel extends Component<Props> {
-	onClick = (e: React.MouseEvent, square: CampaignMapSquareModel) => {
+	onClick = (e: React.MouseEvent, region: RegionModel | null) => {
 		e.stopPropagation();
-		const region = this.props.map.regions.find(r => r.id === square.regionID) ?? null;
 		this.props.onSelectRegion(region);
 	};
 
 	render = () => {
 		try {
+			const squares = this.props.map.squares.map(square => {
+				let backgroundColor = 'rgb(255, 255, 255)';
+				let borderColor = 'rgb(255, 255, 255)';
+
+				const region = this.props.map.regions.find(r => r.id === square.regionID) || null;
+				if (region) {
+					backgroundColor = region.color;
+					borderColor = region.color;
+
+					if (this.props.selectedRegion && (this.props.selectedRegion.id === region.id)) {
+						backgroundColor = 'rgb(255, 255, 255)';
+						borderColor = 'rgb(255, 255, 255)';
+					}
+				}
+
+				const points = [
+					{ x: square.x, y: square.y },
+					{ x: square.x + 1, y: square.y },
+					{ x: square.x + 1, y: square.y + 1 },
+					{ x: square.x, y: square.y + 1 }
+				];
+
+				return (
+					<polygon
+						key={`${square.x} ${square.y}`}
+						className='campaign-map-square'
+						points={points.map(pt => `${pt.x},${pt.y}`).join(' ')}
+						x={square.x}
+						y={square.y}
+						width='1'
+						height='1'
+						style={{
+							fill: backgroundColor,
+							stroke: borderColor
+						}}
+						onClick={e => this.onClick(e, region)}
+					/>
+				);
+			});
+
 			// Get dimensions, adding a 1-square border
 			const dims = CampaignMapLogic.getDimensions(this.props.map);
 			dims.left -= 1;
@@ -29,70 +68,13 @@ export class CampaignMapPanel extends Component<Props> {
 			dims.right += 1;
 			dims.bottom += 1;
 
-			// Determine the percentage width and height of a square
 			const width = 1 + (dims.right - dims.left);
 			const height = 1 + (dims.bottom - dims.top);
-			const squareSizePC = 100 / Math.max(width, height);
-
-			const squares = this.props.map.squares
-				.sort((a, b) => {
-					let result: number = a.x - b.x;
-					if (result === 0) {
-						result = a.y - b.y;
-					}
-					return result;
-				})
-				.map(square => {
-					const region = this.props.map.regions.find(r => r.id === square.regionID);
-					let backgroundColor = 'rgb(255, 255, 255)';
-					let borderColor = 'rgb(255, 255, 255)';
-					if (region) {
-						backgroundColor = region.color;
-						borderColor = 'rgb(0, 0, 0)';
-
-						if (this.props.selectedRegion && (this.props.selectedRegion.id === region.id)) {
-							backgroundColor = 'rgb(255, 255, 255)';
-							borderColor = 'rgb(60, 170, 255)';
-						}
-					}
-
-					// Which borders do we need?
-					const top = this.props.map.squares.find(s => (s.x === square.x) && (s.y === square.y - 1) && (s.regionID !== square.regionID));
-					const right = this.props.map.squares.find(s => (s.x === square.x + 1) && (s.y === square.y) && (s.regionID !== square.regionID));
-					const bottom = this.props.map.squares.find(s => (s.x === square.x) && (s.y === square.y + 1) && (s.regionID !== square.regionID));
-					const left = this.props.map.squares.find(s => (s.x === square.x - 1) && (s.y === square.y) && (s.regionID !== square.regionID));
-
-					const overlap = 0.1;
-
-					return (
-						<div
-							key={`${square.x} ${square.y}`}
-							className='campaign-map-square'
-							style={{
-								width: `calc(${squareSizePC}% + ${overlap * 2}px)`,
-								height: `calc(${squareSizePC}% + ${overlap * 2}px)`,
-								left: `calc(${((square.x - dims.left) * squareSizePC)}% - ${overlap}px)`,
-								top: `calc(${((square.y - dims.top) * squareSizePC)}% - ${overlap}px)`,
-								backgroundColor: backgroundColor,
-								borderTop: top ? `1px solid ${borderColor}` : `0px solid ${backgroundColor}`,
-								borderRight: right ? `1px solid ${borderColor}` : `0px solid ${backgroundColor}`,
-								borderBottom: bottom ? `1px solid ${borderColor}` : `0px solid ${backgroundColor}`,
-								borderLeft: left ? `1px solid ${borderColor}` : `0px solid ${backgroundColor}`
-							}}
-							title={region ? region.name : 'Conquered'}
-							onClick={e => this.onClick(e, square)}
-						/>
-					);
-				});
 
 			return (
-				<div className='campaign-map' onClick={() => this.props.onSelectRegion(null)}>
-					<div className='campaign-map-inner'>
-						<div className='campaign-map-square-container'>
-							{squares}
-						</div>
-					</div>
-				</div>
+				<svg className='campaign-map' viewBox={`${dims.left} ${dims.top} ${width} ${height}`} onClick={e => this.onClick(e, null)}>
+					{squares}
+				</svg>
 			);
 		} catch {
 			return <div className='campaign-map render-error' />;
